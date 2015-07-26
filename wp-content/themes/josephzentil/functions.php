e566a0afae156a812e382a8fa4bef0553984bc0c
Leonardo <leoobjktiv@gmail.com>
2015-07-26 13:11:59 -0400

    remove posts and comments from dashboard

--- wp-content/themes/josephzentil/functions.php
+++ wp-content/themes/josephzentil/functions.php
@@ -11,25 +11,6 @@ register_nav_menus(
 	)
 );
 
-/*-----------------------------------------------------------------------------------*/
-/* Activate sidebar for Wordpress use
-/*-----------------------------------------------------------------------------------*/
-function naked_register_sidebars() {
-	register_sidebar(array(				// Start a series of sidebars to register
-		'id' => 'sidebar', 					// Make an ID
-		'name' => 'Sidebar',				// Name it
-		'description' => 'Take it on the side...', // Dumb description for the admin side
-		'before_widget' => '<div>',	// What to display before each widget
-		'after_widget' => '</div>',	// What to display following each widget
-		'before_title' => '<h3 class="side-title">',	// What to display before each widget's title
-		'after_title' => '</h3>',		// What to display following each widget's title
-		'empty_title'=> '',					// What to display in the case of no title defined for a widget
-		// Copy and paste the lines above right here if you want to make another sidebar,
-		// just change the values of id and name to another word/name
-	));
-}
-// adding sidebars to Wordpress (these are created in functions.php)
-add_action( 'widgets_init', 'naked_register_sidebars' );
 
 /*-----------------------------------------------------------------------------------*/
 /* Enqueue Styles and Scripts
@@ -108,3 +89,15 @@ add_action( 'wp_enqueue_scripts', 'naked_scripts' ); // Register this fxn and al
     ));
   }
   add_action( 'init', 'create_gallery_post' );
+
+
+  function remove_menus () {
+  global $menu;
+  	$restricted = array(__('Posts'),  __('Comments'));
+  	end ($menu);
+  	while (prev($menu)){
+  		$value = explode(' ',$menu[key($menu)][0]);
+  		if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+  	}
+  }
+  add_action('admin_menu', 'remove_menus');
---
+++ wp-content/themes/josephzentil/template-home.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Home page */ ?>
+
+<?php get_header();?>
+
+
+	<div class="container v-align-wrap">
+		<div class="v-align-middle">
+			<div class="columns four">
+				<a href="photo/">Photo</a>
+			</div>
+			<div class="columns four">
+				<a href="photo/">Video</a>
+			</div>
+			<div class="columns four">
+				<a href="photo/">Development</a>
+			</div>
+		</div>
+	</div><!-- #container .content-area -->
+
+<?php get_footer(); // This fxn gets the footer.php file and renders it ?>
