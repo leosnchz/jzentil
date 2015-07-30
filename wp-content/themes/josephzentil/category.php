2f78e91cb4e787c4f7a2e435c9fe92388c165bb4
Leonardo <leoobjktiv@gmail.com>
2015-07-30 18:55:38 -0400

    category layout

--- wp-content/themes/josephzentil/category.php
+++ wp-content/themes/josephzentil/category.php
@@ -4,7 +4,7 @@ get_header();
 $cat_id = get_query_var('cat');
 $cat_name = get_query_var('category_name');
 $args = array(
-  'posts_per_page' => 2,
+  'posts_per_page' => 12,
   'cat' => $cat_id,
   'category_name' => $cat_name,
   'post_type' => 'photo',
@@ -16,41 +16,33 @@ $count = 0;
 
 get_sidebar('galleries'); ?>
 <!-- temporary stylesheet for visual -->
-<link rel="stylesheet" href="http://josephzentil.leonardo-sanchez.com/html/assets/stylesheets/screen.css" />
-<div class="col-md-10">
-  <div class="row">
-    <div class="col-md-6">
-      <h2><?php single_cat_title(); ?></h2>
-    </div>
-    <div class="col-md-6">
+<div class="container">
+    <div class="col-12">
       <?php
         wp_pagenavi( array( 'query' => $photos ) );
-
       ?>
     </div>
-  </div>
-  <div class="row">
-    <ul class="photo-gallery">
+ </div>
+
+ <div class="container">
+    <div class="p-gallery">
     <?php while($photos->have_posts()) : $photos->the_post();
       $count ++;
       $img_src = wp_get_attachment_image_src((int)get_post_meta(get_the_ID())['photo'][0], 'full')[0];
 
       ?>
-      <li class="col-md-3">
+      <div class="p-gal-item">
         <a href="<?php the_permalink(); ?>">
           <img src="<?php echo $img_src; ?>" >
         </a>
-      </li>
+    </div>
       <?php if ( $count % 4 == 0 ) echo '<div class="clearfix"></div>'; ?>
-    <?php endwhile;
+    <?php endwhile; ?>
 
-    ?>
-
-    </ul>
-  </div>
 </div>
 <div class="clearfix"></div>
 
 <?php
 wp_reset_postdata();
 get_footer();
+?>
